133213609c133bade356703c8945f58e8778ebef
sminth <emmanuelmalan225@gmail.com>
2021-03-17 21:13:16 +0000

    .

--- public/js/app.js
+++ public/js/app.js
@@ -49946,7 +49946,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nValidationError: Invalid options object. Sass Loader has been initialized using an options object that does not match the API schema.\n - options has an unknown property 'outputStyle'. These properties are valid:\n   object { implementation?, sassOptions?, prependData?, sourceMap?, webpackImporter? }\n    at validate (C:\\laragon\\www\\never_give_up\\node_modules\\schema-utils\\dist\\validate.js:98:11)\n    at Object.loader (C:\\laragon\\www\\never_give_up\\node_modules\\sass-loader\\dist\\index.js:36:28)\n    at C:\\laragon\\www\\never_give_up\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\laragon\\www\\never_give_up\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\laragon\\www\\never_give_up\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at runSyncOrAsync (C:\\laragon\\www\\never_give_up\\node_modules\\loader-runner\\lib\\LoaderRunner.js:143:3)\n    at iterateNormalLoaders (C:\\laragon\\www\\never_give_up\\node_modules\\loader-runner\\lib\\LoaderRunner.js:232:2)\n    at C:\\laragon\\www\\never_give_up\\node_modules\\loader-runner\\lib\\LoaderRunner.js:205:4\n    at C:\\laragon\\www\\never_give_up\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:75:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
@@ -49957,8 +49957,8 @@ throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\never_give_up\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\never_give_up\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\personnel\Desktop\labo\pomato\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\personnel\Desktop\labo\pomato\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
